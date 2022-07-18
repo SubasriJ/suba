f27bd30422cff7c2a408a88fd19b89357a5ba300
SubasriJ <103495901+SubasriJ@users.noreply.github.com>
2022-07-18 22:33:01 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 function onClickMenu(){
     document.getElementById("menu").classList.toggle("change");
     document.getElementById("nav").classList.toggle("change");
+    document.getElementById("contact").classList.toggle("change");
+    document.getElementById("overlay").classList.toggle("change");
     document.getElementById("menu-bg").classList.toggle("change-bg");
 }
+
